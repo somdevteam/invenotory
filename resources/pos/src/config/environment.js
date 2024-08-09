68eb99055d5a276756b6b5e026164baf0b18e7be
Sadam Abdi <findpay166@gmail.com>
2024-08-09 10:56:45 +0300

    improvement

--- resources/pos/src/config/environment.js
+++ resources/pos/src/config/environment.js
@@ -1,3 +1,4 @@
 export const environment = {
-    URL: window.location.protocol + '//' + window.location.hostname,
+    // URL: window.location.protocol + '//' + window.location.hostname,
+    URL: "http://127.0.0.1:8000"
 };
